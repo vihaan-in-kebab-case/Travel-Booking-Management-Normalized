6ed69e6197f384fcb9efe61761da9699ff410481
Vihaan Kharia <vihaankharia8@gmail.com>
2026-04-09 19:05:42 +0530

    final final

---
+++ frontend/src/utils/storage.js
@@ -0,0 +1,19 @@
+const STORAGE_KEYS = [
+  "users",
+  "travel_mode",
+  "operator",
+  "location",
+  "passenger",
+  "vehicle",
+  "route",
+  "route_stop",
+  "schedule",
+  "booking",
+  "booking_passenger",
+  "payment",
+  "cancellation"
+];
+
+export function clearMockStorage() {
+  STORAGE_KEYS.forEach((key) => localStorage.removeItem(key));
+}
